0c60a97e048972713d0389d1852f516b4ba4154a
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-05-15 08:44:51 +0300

    writting basic loops with the for and while constructs

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,32 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count =10
+    while count >0:
+        print(count )
+        count -=1
+        
+    print("Happy New Year!")
+
+# happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_array = [pow(int_list[i], 2) for i in range(len(int_list))]
+    return new_array
+          
+print(square_integers([1,2,3,4,5]))
+
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        if i %3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i %3 != 0 and i % 5 == 0:
+            print("Buzz")
+        elif i %3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i) 
+fizzbuzz()
